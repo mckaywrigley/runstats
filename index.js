5fdec27704608807e37f25e6b15cbb469b4f8f64
Mckay <mckaywrigley@gmail.com>
2019-05-25 13:27:15 -0700

    sets up initiall express server

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is runnign on port: ${port}.`);
+});
