ca9b0fc1f5a835e27796b05b71de60846ad75bcb
Mckay <mckaywrigley@gmail.com>
2019-05-25 13:53:11 -0700

    connects to MongoDB

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
 const app = express();
+
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(res => console.log(`Connected to MongoDB.`));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
