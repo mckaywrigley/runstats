7f38df38a6bd9d6b84478a0cb4b68dd0c65f9fd7
Mckay <mckaywrigley@gmail.com>
2019-05-25 16:14:27 -0700

    creates PUT user route

--- routes/users.js
+++ routes/users.js
@@ -35,4 +35,23 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.put('/:id', (res, res) => {
+    const { id } = req.params;
+    User.findOneAndUpdate({_id: id})
+    .then(user => {
+        if (user) {
+          return res.status(200).json(user);
+        } else {
+          return res
+            .status(404)
+            .json({ message: "User with that ID could not be found." });
+        }
+      })
+      .catch(err => {
+        return res.status(500).json(err);
+      });
+})
+
+router.delete('/:id', (res, res) => {})
+
 module.exports = router;
