aa098eab3dd37b656045c1c4757e6bd324eed281
Mckay <mckaywrigley@gmail.com>
2019-06-02 22:03:38 -0700

    changes chart style

--- client/src/components/dashboard/LineChart.js
+++ client/src/components/dashboard/LineChart.js
@@ -24,7 +24,7 @@ class LineChart extends Component {
     const myChartRef = this.chartRef.current.getContext("2d");
 
     new Chart(myChartRef, {
-      type: "line",
+      type: "bar",
       data: {
         labels: [
           "5/20/19",
@@ -37,14 +37,36 @@ class LineChart extends Component {
         ],
         datasets: [
           {
-            label: "Miles Ran",
-            data: [3, 7, 5, 4, 0, 10, 0]
+            label: "Miles Run",
+            data: [3, 7, 5, 4, 0, 10, 0],
+            fill: false,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(201, 203, 207, 0.2)"
+            ],
+            borderColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)"
+            ],
+            borderWidth: 1
           }
         ]
       },
       options: {}
     });
   }
+
+  // new Chart(document.getElementById("chartjs-1"),{"type":"bar","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"backgroundColor":,,"}]},"options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}});
 
   lastWeekMiles = () => {
     const lastWeekMilesArr = [];
